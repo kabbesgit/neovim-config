553d7e15b63329f64d7d79b908e7e5ae0cb71938
Kasper Blom <kasper.blom@backlight.co>
2025-11-03 14:07:06 +0100

    Changed colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,18 +1,11 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('tokyonight').setup({
-        style = 'storm',
-        light_style = 'day',
-        styles = {
-          comments = { italic = true },
-          keywords = { bold = true },
-        },
-      })
-      vim.cmd.colorscheme('tokyonight-storm')
-    end,
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "day",
   },
+  config = function()
+    vim.cmd [[colorscheme tokyonight]]
+  end,
 }
